13c6d4d8ccd60bfb6acd501c7d95e815137dcdd3
denny_huang <denny_huang@asgard.com.te>
2023-03-27 10:53:28 +0800

    sort code and  add chkMessage

--- src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
+++ src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
@@ -21,25 +21,38 @@ public class KafkaProducerConfig {
 	//yml設定時備用
 	//@Value("${spring.kafka.bootstrap-servers}")
 	//private String bootstrapServers;
-	public static final String JSON_TOPIC = "test";
-    public static final String DEFAULT_SERVER = "192.168.51.204:29003";
+	public static final String JSON_TOPIC = "kafkaTest1";
+    public static final String DEFAULT_SERVER = "192.168.51.204:29004";
 
     @Bean
-    public ProducerFactory<String, UserVo> producerFactory() {
+    public Map<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, DEFAULT_SERVER);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
-        return new DefaultKafkaProducerFactory<>(config);
+        return config;
     }
 
+    /**
+     * 提供UserVo形式傳遞消息的bean
+     * @return
+     */
     @Bean
     public KafkaTemplate<String, UserVo> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerFactory()));
     }
 
+    /**
+     * 提供String形式傳遞消息的bean
+     * @return
+     */
+    @Bean
+    public KafkaTemplate<String, String> messageTemplate() {
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerFactory()));
+    }
+    
     /**
      * 自動註冊 KafkaAdmin，自動將我們設定的 NewTopic Bean 新增到 Kafka Server 的 Topic
      * @return
--- src/main/java/com/kafka/producer/controller/ProducerController.java
+++ src/main/java/com/kafka/producer/controller/ProducerController.java
@@ -3,6 +3,7 @@ package com.kafka.producer.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,24 +13,37 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kafka.producer.config.KafkaProducerConfig;
 import com.kafka.producer.vo.UserVo;
 
+@Lazy
 @RestController
 @RequestMapping("kafka")
 public class ProducerController {
 
 	 private static final Logger LOGGER = LogManager.getLogger(ProducerController.class);
 
-	    @Autowired
-	    private KafkaTemplate<String, UserVo> kafkaTemplate;
-
-	    @GetMapping("/publish/{dpt}/{id}")
-	    public String post(@PathVariable("dpt") final String dpt, @PathVariable("id") final String id) {
-
-	        UserVo userVo = new UserVo(id, dpt);
-
-	        kafkaTemplate.send(KafkaProducerConfig.JSON_TOPIC, userVo);
-
-
-	        return "Published done";
-	    }
-	
+	 @Autowired
+	 private KafkaTemplate<String, UserVo> kafkaTemplate;
+	 
+	 @Autowired
+	 private KafkaTemplate<String, String> messageTemplate;
+	 
+	 @GetMapping("/publish")
+	 public String post() {
+		 System.out.println("1234567890............");
+		 UserVo userVo = new UserVo("007", "hr");
+		 
+		 kafkaTemplate.send(KafkaProducerConfig.JSON_TOPIC, userVo);
+		 
+		 
+		 return "Published done";
+	 } 
+	 
+	 @GetMapping("/chkMessage")
+	 public String message() {
+		 System.out.println("run chkMessage");
+		 
+		 messageTemplate.send(KafkaProducerConfig.JSON_TOPIC, "test message...");
+		 
+		 
+		 return "Published done";
+	 } 
 }
